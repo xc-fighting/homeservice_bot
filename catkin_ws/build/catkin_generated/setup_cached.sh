021e8f5728fdabfd8ea347146dcb20f1f516c4bd
ChenXuOffWorld <chen.xu@offworld.ai>
2021-01-04 00:08:58 -0800

    First commit

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export TURTLEBOT_GAZEBO_MAP_FILE='/home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/maps/playground.yaml'
+export TURTLEBOT_GAZEBO_WORLD_FILE='/home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world'
+export TURTLEBOT_STAGE_MAP_FILE='/home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/maze.yaml'
+export TURTLEBOT_STAGE_WORLD_FILE='/home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot_simulator/turtlebot_stage/maps/stage/maze.world'
+export TURTLEBOT_STDR_MAP_FILE='/home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot_simulator/turtlebot_stdr/maps/sparse_obstacles.yaml'
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/offworld/Desktop/homeservice/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/offworld/Desktop/homeservice/catkin_ws/build'
+export PYTHONPATH='/home/offworld/swarm-robotic-mining/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/home/offworld/swarm-robotic-mining/dev_ws/src/firmware/bot_firmware_apis:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_mobilebase_api/build:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_overseer_api:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_database:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_derived_tm:/home/offworld/swarm-robotic-mining/dev_ws/src:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_can:/home/offworld/offworld_code/common:/home/offworld/offworld_code/offworld-ur:/home/offworld/swarm-robotic-mining/catkin_ws/src/bot_panel_manager/scripts/bot_panel_manager:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_firmware:/home/offworld/swarm-robotic-mining/dev_ws/src/firmware/bot_firmware_apis:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_mobilebase_api/build:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_overseer_api:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_database:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_derived_tm:/home/offworld/swarm-robotic-mining/dev_ws/src:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_can:/home/offworld/offworld_code/common:/home/offworld/offworld_code/offworld-ur:/home/offworld/swarm-robotic-mining/catkin_ws/src/bot_panel_manager/scripts/bot_panel_manager:/home/offworld/swarm-robotic-mining/dev_ws/src/bot_firmware/bot_firmware_plugins'
---
+++ catkin_ws/build/turtlebot/turtlebot_description/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/offworld/Desktop/homeservice/catkin_ws/src/turtlebot/turtlebot_description
+# Build directory: /home/offworld/Desktop/homeservice/catkin_ws/build/turtlebot/turtlebot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_turtlebot_description_gtest_turtlebot_description_test_urdf "/home/offworld/Desktop/homeservice/catkin_ws/build/catkin_generated/env_cached.sh" "/home/offworld/ve/py27/bin/python2" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/offworld/Desktop/homeservice/catkin_ws/build/test_results/turtlebot_description/gtest-turtlebot_description_test_urdf.xml" "--return-code" "/home/offworld/Desktop/homeservice/catkin_ws/devel/lib/turtlebot_description/turtlebot_description_test_urdf --gtest_output=xml:/home/offworld/Desktop/homeservice/catkin_ws/build/test_results/turtlebot_description/gtest-turtlebot_description_test_urdf.xml")
